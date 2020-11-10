2ca62085cc05b9a6dd0f0b71404fb8cb9b75a898
Work PC <antariuz@gmail.com>
2020-11-10 10:50:26 +0300

    Initial commit

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data @ToString
+public class Car {
+
+    private String brand;
+    private String model;
+    private Integer manufacturedYear;
+
+}
